3fa59fb02681dc6bd1e71563361b73715899332a
Raghu Maharshi <132325102+raghu024@users.noreply.github.com>
2023-05-10 15:55:08 +0530

    Add files via upload

---
+++ week-5.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#Define Two vectors
+
+vector1=np.array([1,2,3])
+vector2=np.array([4,5,6])
+
+#crossProuct
+
+cross_product=np.cross(vector1,vector2)
+
+print("the cross product of",vector1,"and",vector2,"is",cross_product)
